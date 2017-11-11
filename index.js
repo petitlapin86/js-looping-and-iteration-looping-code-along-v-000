cec4fd89cceb86d570bb2ae523cc0645ebd2d95b
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-11 16:33:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ return names;
 
 
 
+function tailsNeverFails () {
+  let tails = 0;
 
-  function tailsNeverFails (tails) {
- while (let tails = 0; Math.random() >= 0.5; tails++) {
- }
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
 
- return return `You got ${tails} tails in a row!`;
- }
+  return `You got ${tails} tails in a row!`;
+}

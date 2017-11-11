0572d514424aaaab8be1e37fa44630080a829a26
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-11 16:34:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+ function printBadges (names) {
+for (let counter = 0; counter < names.length; counter++) {
+  console.log(`Welcome ${names[counter]}! You are employee #${counter + 1}.`);
+}
+
+return names;
+}
+
+
+
+
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}

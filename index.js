da8ddbcbff593f3478ca13a92fe05a743574a300
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-11 16:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solutions in this file
- printBadges (['Ada', 'Brendan', 'Ali']) {
-
+ printBadges (['Ada', 'Brendan', 'Ali']); {
 
 for (let counter = 0; counter<3; counter++;) {
   console.log('Welcome ${counter} you are employee');
